0156eb767479682e5b27e74ec104ffbb3b8c973f
BuildTools <unconfigured@null.spigotmc.org>
2018-11-17 12:11:19 -0500

    original

---
+++ 8143-rover-ruckus/FtcRobotController/src/main/java/ftc/vision/FrameGrabber.java
@@ -0,0 +1,68 @@
+package ftc.vision;
+
+import android.view.SurfaceView;
+import android.view.View;
+
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+
+public class FrameGrabber implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    //VARIABLES
+    private final static String TAG = "FrameGrabber";
+    private Mat frame;
+    private ImageProcessor processor;
+    private boolean mask = true;
+
+    //CONSTRUCTOR
+    public FrameGrabber (CameraBridgeViewBase cameraView, int frameWidth, int frameHeight) {
+        //make camera view visible
+        cameraView.setVisibility(SurfaceView.VISIBLE);
+
+        //set dimensions of the frame
+        cameraView.setMinimumHeight(frameHeight);
+        cameraView.setMinimumWidth(frameWidth);
+        cameraView.setMaxFrameSize(frameWidth, frameHeight);
+
+        cameraView.setCvCameraViewListener(this);
+    }
+
+    public void setProcessor(ImageProcessor processor) {
+        this.processor = processor;
+    }
+
+    public boolean isProcessorNull() {
+        return processor == null;
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        frame = new Mat(height, width, CvType.CV_8UC4, new Scalar(0, 0, 0));
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        frame = ImUtil.rotate(inputFrame.rgba(), 90);
+        processFrame();
+        return frame;
+    }
+
+    public Result processFrame() {
+        if (isProcessorNull()) {
+            return null;
+        }
+        Result result = processor.process(frame);
+
+        if (mask) {
+            frame = result.getFrame();
+        }
+        return result;
+    }
+}
---
+++ 8143-rover-ruckus/FtcRobotController/src/main/java/ftc/vision/ImageProcessor.java
@@ -0,0 +1,6 @@
+package ftc.vision;
+import org.opencv.core.Mat;
+
+public interface ImageProcessor<ResultType> {
+    Result<ResultType> process (Mat rgbaFrame);
+}
